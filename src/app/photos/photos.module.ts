61a2c32a89ad34aca7d8ec8804fb83b1fc6303da
Ricardo Ulrich Bomfim <c032550@corp.caixa.gov.br>
2019-07-05 09:56:32 -0300

    aula03-05 - import common module on photo module

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
@@ -9,6 +10,9 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
         PhotoComponent, 
         PhotoListComponent 
     ],
-    imports: [ HttpClientModule ]
+    imports: [ 
+        CommonModule,
+        HttpClientModule
+    ]
 })
 export class PhotosModule {}
