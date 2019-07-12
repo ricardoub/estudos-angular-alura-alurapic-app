b7b9982c6052edd8cedaf9ea490d9f2d0eddd1af
Ricardo Ulrich Bomfim <c032550@corp.caixa.gov.br>
2019-07-12 10:18:48 -0300

    aula.06-05 - resolver photos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
 
 
@@ -12,20 +12,15 @@ import { Photo } from '../photo/photo';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filter: string = '';
 
   constructor(
-    private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-
-    const userName = this.activatedRoute.snapshot.params.userName;
-
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
-
+    //this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.photos = this.activatedRoute.snapshot.data.photos;
   }
 
 }
