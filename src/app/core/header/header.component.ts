4ed8dd44228a7863d3fae2602b4457fc257931b3
Ricardo Ulrich Bomfim <c032550@corp.caixa.gov.br>
2019-07-23 09:42:00 -0300

    p2-aula03-07 - logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,8 +14,14 @@ export class HeaderComponent {
     user$: Observable<User>;
     
     constructor (
-        userService: UserService
+        private userService: UserService,
+        private router: Router
     ) {
-        this.user$ = userService.getUser();
+        this.user$ = this.userService.getUser();
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
